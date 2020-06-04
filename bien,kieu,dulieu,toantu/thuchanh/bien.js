abc5a48126aeeccfdd65668d7e7d52f60b311cf3
unknown <nptqn92@gmail.com>
2020-06-04 13:02:21 +0700

    upload bai tap

---
+++ bien,kieu,dulieu,toantu/thuchanh/bien.js
@@ -0,0 +1,8 @@
+let inputwwidth = 10;
+let inputheight = 20;
+let inputWidth = prompt("nhap width");
+let inputHeight =prompt("nhap height");
+let Width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = Width*height;
+document.write('area =' + area);
